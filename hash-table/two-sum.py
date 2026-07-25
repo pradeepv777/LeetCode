85348cd33b092eacb045e9435ac536802e9bf6e6
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-07-26 01:16:04 +0530

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- hash-table/two-sum.py
+++ hash-table/two-sum.py
@@ -1,5 +1,5 @@
-class Solution(object):
-    def twoSum(self, nums, target):
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen= {}
         for i,num in enumerate(nums):
             diff = target - num
