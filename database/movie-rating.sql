218aa13e1065f19f08251320a2b9c87edcd319c9
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-07-26 23:19:14 +0530

    LC-movie-rating [Runtime: N/A; Memory: N/A]

--- database/movie-rating.sql
+++ database/movie-rating.sql
@@ -1,3 +1,4 @@
+(
 select u.name as results
 from users u
 join movierating mr
@@ -5,9 +6,11 @@ on u.user_id = mr.user_id
 group by u.user_id, u.name
 order by count(*) desc, u.name
 limit 1
+)
 
 union all
 
+(
 select m.title as results
 from movies m
 join movierating mr
@@ -15,4 +18,5 @@ on m.movie_id = mr.movie_id
 where created_at between '2020-02-01' and '2020-02-29'
 group by m.movie_id, m.title
 order by avg(rating) desc, m.title
-limit 1;
+limit 1
+);
