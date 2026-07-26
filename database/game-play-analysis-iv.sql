81698f662a08a11f39b7efe43cdfd671b1993b4b
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-07-26 22:06:55 +0530

    LC-game-play-analysis-iv [Runtime: N/A; Memory: N/A]

---
+++ database/game-play-analysis-iv.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+select
+round(
+count(distinct a.player_id) /
+(select count(distinct player_id) from activity),
+2
+) as fraction
+from activity a
+join (
+select
+player_id,
+min(event_date) as first_login
+from activity
+group by player_id
+) b
+on a.player_id = b.player_id
+and a.event_date = date_add(b.first_login, interval 1 day);
