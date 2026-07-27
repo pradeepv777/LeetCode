acd72b49d31ecf77dc75de58d657e7cfebeb4205
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-07-27 11:24:16 +0530

    LC-maximum-product-of-two-elements-in-an-array [Runtime: N/A; Memory: N/A]

--- sorting/maximum-product-of-two-elements-in-an-array.py
+++ sorting/maximum-product-of-two-elements-in-an-array.py
@@ -9,11 +9,11 @@ class Solution:
             if nums[i] > max1:
                 max2 = max1
                 max2id = max1id
-                
                 max1 = nums[i]
                 max1id = i
+
             elif nums[i] > max2:
                 max2 = nums[i]
                 max2id = i
 
-        return (nums[max1id] - 1) * (nums[max2id] - 1)
+        return (nums[max1id]-1)*(nums[max2id]-1)
