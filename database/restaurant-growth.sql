9407351e8ae32754df95b1a2bac5c2a423b2093f
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-07-27 22:38:52 +0530

    LC-restaurant-growth [Runtime: N/A; Memory: N/A]

---
+++ database/restaurant-growth.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+select
+visited_on,
+amount,
+round(amount / 7, 2) as average_amount
+from (
+select
+visited_on,
+sum(sum(amount)) over (
+order by visited_on
+rows between 6 preceding and current row
+) as amount,
+row_number() over (order by visited_on) as rn
+from customer
+group by visited_on
+) t
+where rn >= 7;
