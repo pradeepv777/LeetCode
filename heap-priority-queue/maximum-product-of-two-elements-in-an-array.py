0b8e88361ee4943dc0fd25ada3faeba0127c424a
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-07-27 11:16:20 +0530

    LC-maximum-product-of-two-elements-in-an-array [Runtime: N/A; Memory: N/A]

---
+++ heap-priority-queue/maximum-product-of-two-elements-in-an-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        max1 = float("-inf")
+        max2 = float("-inf")
+        max1id = -1
+        max2id = -1
+        
+        for i in range(len(nums)):
+            if nums[i] > max1:
+                max2 = max1
+                max2id = max1id
+                
+                max1 = nums[i]
+                max1id = i
+            elif nums[i] > max2:
+                max2 = nums[i]
+                max2id = i
+
+        return (nums[max1id] - 1) * (nums[max2id] - 1)
