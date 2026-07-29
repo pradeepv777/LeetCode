0c4cfc1a6a7d3da8b5809e05e1f653ef455b4399
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-07-29 12:00:06 +0530

    LC-valid-palindrome [Runtime: N/A; Memory: N/A]

--- two-pointers/valid-palindrome.py
+++ two-pointers/valid-palindrome.py
@@ -1,11 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         pal = ""
-        palrev = ""
         for i in s:
             if i.isalnum():
                 pal += i.lower()
-        for i in pal :
-            palrev = i+palrev
-        return palrev == pal
+        return pal[::-1] == pal
         
