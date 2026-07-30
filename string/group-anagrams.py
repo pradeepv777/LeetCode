2e2ff04139c9835d570536c8a1cb555698b1df7a
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-07-30 19:58:08 +0530

    LC-group-anagrams [Runtime: N/A; Memory: N/A]

--- string/group-anagrams.py
+++ string/group-anagrams.py
@@ -1,11 +1,12 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         seen = {}
         for i in strs:
             key = "".join(sorted(i))
-
             if key not in seen:
-                seen[key]=[]
-            seen[key].append(i)
-
+                seen[key] = []
+            else:
+                seen[key].append = i
         return list(seen.values())
+
+        
