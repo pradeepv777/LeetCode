d76ee1864a373ff7a75e2459b91d9e66c3c9eb29
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-07-30 18:38:57 +0530

    LC-continuous-subarray-sum [Runtime: N/A; Memory: N/A]

---
+++ math/continuous-subarray-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        prefix = 0
+        seen = {0:-1}
+        for i in range(len(nums)):
+            prefix+=nums[i]
+            rem = prefix%k
+            if rem in seen:
+                if i - seen[rem]>=2:
+                    return True
+                else:
+                    seen[rem] = i
+        return False
+        
