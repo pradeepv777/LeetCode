be5c455382f443df5352fce441dba8377dfca5b5
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-07-31 09:40:14 +0530

    LC-happy-number [Runtime: 0 ms; Memory: 19.3 MB]

--- two-pointers/happy-number.py
+++ two-pointers/happy-number.py
@@ -1,16 +1,19 @@
-class Solution(object):
-    def isHappy(self, n):
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        sets = set()
 
-        seen = set()
-        while n != 1 and n not in seen:
-            seen.add(n)
+        while n!=1 and n not in sets:
+            sets.add(n)
             total = 0
 
-            while n > 0:
-                digit = n % 10
-                total += digit * digit
-                n //= 10
+            while n>0:
+                digit = n%10
+                total+= digit*digit
+                n//=10
 
             n = total
+        return n==1
 
-        return n == 1
+
+
+        
