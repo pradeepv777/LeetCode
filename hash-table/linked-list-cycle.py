3d16ac4f7b8583967ae0a829506b130973b74877
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-07-31 09:46:12 +0530

    LC-linked-list-cycle [Runtime: N/A; Memory: N/A]

--- hash-table/linked-list-cycle.py
+++ hash-table/linked-list-cycle.py
@@ -10,7 +10,7 @@ class Solution:
         fast = head
         while fast and fast.next:
             slow = slow.next
-            fast = fast.next
+            fast = fast.next.next
             if slow == fast:
                 return True
         return False
