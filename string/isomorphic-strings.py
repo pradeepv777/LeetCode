47287514a787ee0eb4111c83c9c6f1738f0389c1
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-07-31 09:22:22 +0530

    LC-isomorphic-strings [Runtime: N/A; Memory: N/A]

--- string/isomorphic-strings.py
+++ string/isomorphic-strings.py
@@ -1,7 +1,20 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        seenS = Counter(s)
-        seenT = Counter(t)
-        return list(seenS.values())== list(seenT.values())
+        seenS = {}
+        seenT = {}
+
+        for i in range(len(s)):
+            if s[i] in seenS:
+                if seenS[s[i]]!= t[i]:
+                    return False
+            else:
+                seenS[s[i]] = t[i]
+
+            if t[i] in seenT:
+                if seenT[t[i]]!=  s[i]:
+                    return False
+            else:
+                seenT[t[i]] = s[i]
+        return True
 
         
