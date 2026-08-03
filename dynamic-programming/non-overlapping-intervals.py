6fb8841d00df417d9309c4def37261775e2c1477
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-03 16:51:23 +0530

    LC-non-overlapping-intervals [Runtime: N/A; Memory: N/A]

--- dynamic-programming/non-overlapping-intervals.py
+++ dynamic-programming/non-overlapping-intervals.py
@@ -1,7 +1,6 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         intervals.sort(key = lambda x: x[1])
-        merged = [intervals[0]]
         prev_end = intervals[0][1]
         count = 0
         for current in intervals[1:]:
