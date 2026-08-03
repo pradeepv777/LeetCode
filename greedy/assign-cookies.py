4b4b8c5f1f9a566240283c285222cd4e4a7514b6
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-03 12:23:45 +0530

    LC-assign-cookies [Runtime: N/A; Memory: N/A]

---
+++ greedy/assign-cookies.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g = sorted(g)
+        s = sorted(s)
+        i = 0
+        j = 0
+        count=0
+
+        while i<len(g) and j<len(s):
+            if g[i] <= s[j]:
+                count+=1
+                i+=1
+                j+=1
+
+            else :
+                i+=1
+
+        return count
+
+
+
+
