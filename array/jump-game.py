7e26cdc50fa35882ea7d568196f76ea17384f707
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-03 13:49:26 +0530

    LC-jump-game [Runtime: N/A; Memory: N/A]

--- array/jump-game.py
+++ array/jump-game.py
@@ -1,6 +1,6 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        farthest = nums[0] 
+        farthest = 0
         for i in range(len(nums)):
             if i > farthest:
                 return False
