a806ca0cadcf08c75d09bb328fb1717f7bfd722f
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-03 13:23:22 +0530

    LC-lemonade-change [Runtime: N/A; Memory: N/A]

---
+++ array/lemonade-change.py
@@ -0,0 +1,30 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        five = 0
+        ten =0
+        for i in range(len(bills)):
+            if bills[i] == 5:
+                five+=1
+
+            elif bills[i] == 10 :
+                if five == 0:
+                    return False
+                ten+=1
+                five-=1
+
+            else:
+                if ten>0 and five>0:
+                    ten-=1
+                    five-=1
+                elif ten> 0 and five == 0:
+                    return False
+                elif ten == 0 and five>3:
+                    five -=3
+        return True
+
+
+
+
+
+
+        
