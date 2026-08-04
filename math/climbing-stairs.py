8ecad3ad945a8cdabc9cfb1ec184e5fa02a94f37
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-04 16:59:48 +0530

    LC-climbing-stairs [Runtime: N/A; Memory: N/A]

--- math/climbing-stairs.py
+++ math/climbing-stairs.py
@@ -1,13 +1,16 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        memo = {}
+        def dfs(n):
+                if n == 1:
+                    return 1
 
-        if n == 1:
-            return 1
+                if n == 2:
+                    return 2
 
-        if n == 2:
-            return 2
-
-        return self.climbStairs(n-1) + self.climbStairs(n-2)
+                memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+                return memo[n]
+        return dfs(n)
 
         
         
