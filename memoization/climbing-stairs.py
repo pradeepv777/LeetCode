ad6f05326cc794183d8485fc7c657e9790469871
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-04 17:00:52 +0530

    LC-climbing-stairs [Runtime: N/A; Memory: N/A]

--- memoization/climbing-stairs.py
+++ memoization/climbing-stairs.py
@@ -2,12 +2,13 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         memo = {}
         def dfs(n):
+                if n in memo:
+                    return memo[n]
                 if n == 1:
                     return 1
 
                 if n == 2:
                     return 2
-
                 memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
                 return memo[n]
         return dfs(n)
