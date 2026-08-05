bf43691a4416f1b270cb10671d685469a9479320
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-05 21:18:33 +0530

    LC-scramble-string [Runtime: N/A; Memory: N/A]

--- dynamic-programming/scramble-string.py
+++ dynamic-programming/scramble-string.py
@@ -3,10 +3,25 @@ class Solution:
         if len(s1) != len(s2):
             return False
         memo = {}
-        def dfs(s,t):
-            if sorted(s) != sorted(t):
-                return False
+        
+        def dfs(s, t):
+            if (s, t) in memo:
+                return memo[(s, t)]
             if s == t:
                 return True
+            if sorted(s) != sorted(t):
+                return False
+                
+            n = len(s)
+            for k in range(1, n):
+                if dfs(s[:k], t[:k]) and dfs(s[k:], t[k:]):
+                    memo[(s, t)] = True
+                    return True
+                if dfs(s[:k], t[n-k:]) and dfs(s[k:], t[:n-k]):
+                    memo[(s, t)] = True
+                    return True
+                    
+            memo[(s, t)] = False
+            return False
 
-        return dfs(s1,s2)
+        return dfs(s1, s2)
