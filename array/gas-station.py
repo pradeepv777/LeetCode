173b76d5732660e435d22d3a2d358d36224ce7da
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-07 10:54:10 +0530

    LC-gas-station [Runtime: N/A; Memory: N/A]

---
+++ array/gas-station.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        tank = 0
+        start = 0
+        total = 0
+        for i in range(len(gas)):
+            diff = gas[i]-cost[i]
+            tank+=diff
+            total+=diff
+
+            if tank<0:
+                start = i+1
+                tank = 0
+        if total  < 0:
+            return -1
+
+        return start
+        
