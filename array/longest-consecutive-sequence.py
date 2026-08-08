932bf87b7e363b920b34e351a64920ade664b303
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-08 17:00:02 +0530

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

--- array/longest-consecutive-sequence.py
+++ array/longest-consecutive-sequence.py
@@ -3,12 +3,12 @@ class Solution:
         num_set = set(nums)
         max_length = 0
         for num in nums:
-            if (num-1) not in nums:
+            if (num-1) not in num_set:
                 curr_length = 0
 
                 while curr_length + num in num_set:
                     curr_length +=1
                     max_length = max(curr_length,max_length)
-                    
+
         return max_length
         
