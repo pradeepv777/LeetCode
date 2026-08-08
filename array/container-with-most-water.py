53ae787a2329a4085f6215ba26d3696a79975e08
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-08 22:44:25 +0530

    LC-container-with-most-water [Runtime: N/A; Memory: N/A]

--- array/container-with-most-water.py
+++ array/container-with-most-water.py
@@ -1,15 +1,20 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        left = 0
-        right = len(height) -1
-        maxarea = 0
+        leftmax = 0
+        rightmax= len(height) - 1
+        area = 0
+        max_area = float("-inf")
 
-        while left<right:
-            area = min(height[left],height[right]) * (right - left)
-            maxarea = max(maxarea,area)
-            if height[left]<height[right]:
-                left+=1
+        while leftmax < rightmax:
+            area = min(height[leftmax],height[rightmax]) * (rightmax - leftmax)
+            if height[leftmax] > height[rightmax]:
+                rightmax -=1
             else:
-                right-=1
-        return maxarea
-            
+                leftmax+=1
+            if area > max_area:
+                max_area = area
+                
+        return max_area
+
+
+       
