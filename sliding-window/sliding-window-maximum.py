841dc750380c0d0e48a5820af6a3398e2c9814be
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-11 12:27:17 +0530

    LC-sliding-window-maximum [Runtime: 183 ms; Memory: 34.6 MB]

--- sliding-window/sliding-window-maximum.py
+++ sliding-window/sliding-window-maximum.py
@@ -1,26 +1,26 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        q = collections.deque()
-        left = 0
-        right = 0
-        arr = []
-
-        while right<len(nums):
-
+       q = collections.deque()
+       left = 0
+       right = 0
+       res = []
+       # 1. If deq not empty and last element is less than incoming elment
+       # --> pop the last elments [-1] in deq until true if less append new val  
+       while right<len(nums):
             while q and nums[q[-1]] < nums[right]:
                 q.pop()
             q.append(right)
 
+            # 2. REMOVE EXPIRED ELEMENTS
+            # If the oldest index at the front of the queue falls behind the 
+            # left boundary of our current window, remove it.
             if left > q[0]:
                 q.popleft()
 
-            if (right+1) >= k:
-                arr.append(nums[q[0]])
-                left+=1
-            right+=1
-
-        return arr
-
+            if (right+1)>=k:
+                res.append(nums[q[0]])
+                left+=1 # MOve left ptr to decrease the window size
+            right+=1  # if window size is not k add elemts till it is
+       return res
             
-
-      
+        
