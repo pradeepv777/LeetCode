f7e86a98ea20f34a7d62c12bfa446c3710258522
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-11 12:01:05 +0530

    LC-minimum-window-substring [Runtime: N/A; Memory: N/A]

--- string/minimum-window-substring.py
+++ string/minimum-window-substring.py
@@ -3,7 +3,7 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if not t or not s or len(s)<len(t):
             return ""
-
+        # 1. Declare Variable and get count of our target
         count_t = Counter(t)
         window = {}
         have = 0
@@ -11,11 +11,13 @@ class Solution:
         l = 0
         res = [-1,-1]
         min_len = float('inf')
+        #2. Add elemnts to the window and their count
         for r,char in enumerate(s):
             window[char] = window.get(char,0)+1
 
             if char in count_t and window[char] == count_t[char]:
                 have+=1
+            #3. Shrink the window after the elements are found from left
             while need == have:
                 if (r-l+1) < min_len:
                     res = [l,r]
@@ -25,7 +27,8 @@ class Solution:
                 if left_char in count_t and window[left_char]< count_t[left_char]:
                     have-=1
                 l+=1
-        l,r = res
+
+        l,r = res  # Save the Value of left and right ptr
         return s[l:r+1] if min_len != float('inf') else ""
 
 
