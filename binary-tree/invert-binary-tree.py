ffdee8e769d7fbeab7f70ef5f5087a5009fed499
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-14 09:25:05 +0530

    LC-invert-binary-tree [Runtime: N/A; Memory: N/A]

--- binary-tree/invert-binary-tree.py
+++ binary-tree/invert-binary-tree.py
@@ -1,19 +1,16 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def invertTree(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: Optional[TreeNode]
-        """
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
-            return None
-        root.right,root.left = root.left,root.right
+            return 
+        root.left,root.right = root.right,root.left
         self.invertTree(root.left)
         self.invertTree(root.right)
+
         return root
         
