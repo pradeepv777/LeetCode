48b18cd01be7aeeefec99a8a337fc1f1f43b0108
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-15 11:05:26 +0530

    LC-kth-largest-element-in-a-stream [Runtime: N/A; Memory: N/A]

--- binary-tree/kth-largest-element-in-a-stream.py
+++ binary-tree/kth-largest-element-in-a-stream.py
@@ -1,19 +1,22 @@
 import heapq
+
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        self.heap = []
         self.k = k
+        self.heap = []
+
         for i in nums:
-            heapq.heappush(self.heap,i)
+            heapq.heappush(self.heap, i)
             if len(self.heap) > self.k:
                 heapq.heappop(self.heap)
-                
+
     def add(self, val: int) -> int:
-        heapq.heappush(self.heap,val)
+        heapq.heappush(self.heap, val)
         if len(self.heap) > self.k:
             heapq.heappop(self.heap)
-            return self.heap[0]
+
+        return self.heap[0]
 
         
 
