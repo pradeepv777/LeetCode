c1833c8612881f337126dec5758d79720aeaeab3
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-17 15:26:50 +0530

    LC-house-robber-ii [Runtime: N/A; Memory: N/A]

---
+++ array/house-robber-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums)
+
+        def helper(arr):
+            memo = {}
+            def dfs(i):
+                if i in memo:
+                    return memo[i]
+                if i >= len(arr):
+                    return 0
+
+                memo[i] = max(
+                    arr[i] + dfs(i + 2),
+                    dfs(i + 1)
+                )
+                return memo[i]
+
+            return dfs(0)
+        return max(helper(nums[:-1]), helper(nums[1:]))
