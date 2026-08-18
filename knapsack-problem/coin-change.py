40d7e91a6bd6ec55e7e9a449d752a4ff205b1a26
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-18 14:57:31 +0530

    LC-coin-change [Runtime: N/A; Memory: N/A]

--- knapsack-problem/coin-change.py
+++ knapsack-problem/coin-change.py
@@ -3,12 +3,13 @@ class Solution:
 
         memo = {}
         def dfs(rem):
+            if rem in memo:
+                return memo[rem]
             if rem == 0:
                 return 0
             if rem < 0:
                 return float("inf")
-            if rem in memo:
-                return memo[rem]
+
 
             min_coins = float("inf")
 
