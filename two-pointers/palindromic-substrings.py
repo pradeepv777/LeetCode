7ae9adc06f1d6731889e2edcfbf4fa1f12df7a08
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-18 11:57:07 +0530

    LC-palindromic-substrings [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/palindromic-substrings.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        max_len = 0
+
+        for i in range(len(s)):
+            l,r = i, i
+
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                    max_len += 1
+                    l -=1
+                    r +=1
+
+            l,r = i, i+1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                    max_len += 1
+                    l -=1
+                    r +=1
+        return max_len
+
+        
