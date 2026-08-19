647ef99e65bde3329fec8dcfd87639e825a0e5c1
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-19 10:49:51 +0530

    LC-jump-game [Runtime: N/A; Memory: N/A]

--- greedy/jump-game.py
+++ greedy/jump-game.py
@@ -2,8 +2,14 @@ class Solution:
     def canJump(self, nums: List[int]) -> bool:
         farthest = 0
         for i in range(len(nums)):
-            if i > farthest:
+            if i > farthest :
                 return False
-            farthest = max(farthest, i + nums[i])
+            farthest = max(farthest, nums[i] + i)
+            if farthest >= len(nums):
+                return True
         return True
-        
+
+
+
+            
+
