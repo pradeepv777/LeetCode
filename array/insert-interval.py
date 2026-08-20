60846dfc785a780318f78b1bda296e68ce06e6d6
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-20 11:56:33 +0530

    LC-insert-interval [Runtime: N/A; Memory: N/A]

--- array/insert-interval.py
+++ array/insert-interval.py
@@ -1,17 +1,24 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         res = []
-        for i in range(len(intervals)):
-            
-            if newInterval[1]<intervals[i][0]:
-                res.append(newInterval)
-                return res + intervals[i:]
-            elif newInterval[0]>intervals[i][1]:
-                   res.append(intervals[i])
-            else:
-                newInterval = [min(newInterval[0],intervals[i][0]),max(newInterval[1],intervals[i][1])]
-        res.append(newInterval)
-        return res
+        i = 0
+        n = len(intervals)
 
-        
-        
+        # 1. Add all intervals ending before newInterval starts
+        while i < n and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
+
+        # 2. Merge all overlapping intervals with newInterval
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        res.append(newInterval)
+
+        # 3. Add the rest of the intervals
+        while i < n:
+            res.append(intervals[i])
+            i += 1
+
+        return res
