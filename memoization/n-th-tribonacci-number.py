a21acf84254b5e0069711292f0d4dc7762b80c3a
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-20 15:01:32 +0530

    LC-n-th-tribonacci-number [Runtime: N/A; Memory: N/A]

--- memoization/n-th-tribonacci-number.py
+++ memoization/n-th-tribonacci-number.py
@@ -1,9 +1,16 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if n == 1 or n == 2:
-            return 1
+        memo = {}
+        def tri(n):
+            if n in memo:
+                return memo[n]
+            if n == 0:
+                return 0
+            if n == 1 or n == 2:
+                return 1
+            
+            memo[n] =  tri(n-3) + tri(n-2) + tri(n-1)
+            return memo[n]
 
-        return self.tribonacci(n-3) + self.tribonacci(n-2) + self.tribonacci(n-1)
+        return tri(n)
         
