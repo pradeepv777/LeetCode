14e4358c4dc9b0439a7b279b12f6f5317d2159ea
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-22 12:08:13 +0530

    LC-missing-number [Runtime: 4 ms; Memory: 20.5 MB]

--- sorting/missing-number.py
+++ sorting/missing-number.py
@@ -1,14 +1,9 @@
-class Solution(object):
-    def missingNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        n = len(nums)
-        sums = n*(n+1)//2
-        actual = 0
-        for i in nums:
-            actual+=i
-        return sums-actual
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        num = len(nums)
 
-        
+        for i in range(len(nums)):
+            num ^= i
+            num ^= nums[i]
+
+        return num
