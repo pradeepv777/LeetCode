46b041328f5f2153e912f56f5045e493ec91bc8d
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-22 11:10:00 +0530

    LC-counting-bits [Runtime: N/A; Memory: N/A]

--- bit-manipulation/counting-bits.py
+++ bit-manipulation/counting-bits.py
@@ -2,16 +2,19 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         res = []
         i = 0
-        count = 0
+
         while i <= n:
-            rem = i % 2
-            i = i // 2
-            if rem == 1:
-                count+= 1
+            count = 0
+            num = i
+
+            while num > 0:
+                rem = num % 2
+                num = num // 2
+                if rem == 1:
+                    count+= 1
 
             res.append(count)
-        count = 0
-        i+=1
+            i+=1
 
         return res
         
