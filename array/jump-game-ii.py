e4ec94b29ee8cafb7ce6f0ae525e6eef825916f1
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-26 11:15:32 +0530

    LC-jump-game-ii [Runtime: N/A; Memory: N/A]

---
+++ array/jump-game-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if len(nums) <=1:
+            return 0
+
+        overall_sum = nums[0]
+        min_jumps = 1
+        max_val = float('-inf')
+
+        for i in range(len(nums) -1):
+            max_val = max(max_val,nums[i] + i)
+
+            if i == overall_sum:
+                min_jumps+=1
+                overall_sum = max_val
+
+            if overall_sum >= len(nums)  :
+                break
+
+        return min_jumps
+
+
+
+
+
+      
