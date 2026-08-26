ebd8996d957e4185edf1620dc3c6fb727fd0e487
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-26 11:47:22 +0530

    LC-gas-station [Runtime: N/A; Memory: N/A]

--- array/gas-station.py
+++ array/gas-station.py
@@ -4,16 +4,17 @@ class Solution:
         tank = 0
         start = 0
         total = 0
-        
+
         for i in range(len(gas)):
 
             diff = gas[i] - cost[i]
             tank += diff
             total += diff
 
-            if total < 0 :
+            if tank < 0 :
                 start = i+1
                 tank = 0
 
         if total < 0:
             return -1
+        return start
