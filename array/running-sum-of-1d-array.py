3a3eade8406b3692e0dfb082c87e044cc080daf0
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-26 10:07:52 +0530

    LC-running-sum-of-1d-array [Runtime: N/A; Memory: N/A]

--- array/running-sum-of-1d-array.py
+++ array/running-sum-of-1d-array.py
@@ -1,12 +1,9 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
 
-        res = [1] * len(nums)
-
-        res[0] = nums[0]
         for i in range(1,len(nums)):
-            res[i] = res[i-1] + nums[i]
-        return res
+            nums[i] = nums[i-1] + nums[i]
+        return nums
 
 
         
