31949631ecef72abba95b62339e8565e32d29b85
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-28 11:18:09 +0530

    LC-partition-labels [Runtime: 58 ms; Memory: 19.4 MB]

---
+++ string/partition-labels.py
@@ -0,0 +1,17 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+
+        last_seen = {char: i for i, char in enumerate(s)}
+        
+        res = []
+        left = 0  
+        right = 0    
+        
+        for i, char in enumerate(s):
+            right = max(right, last_seen[char])
+            
+            if i == right:
+                res.append(right - left + 1)
+                left = i + 1  
+
+        return res
