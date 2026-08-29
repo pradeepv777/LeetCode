29f7550d80b09dba774416f035dbb26a974ee54d
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-29 11:26:04 +0530

    LC-reverse-vowels-of-a-string [Runtime: N/A; Memory: N/A]

--- string/reverse-vowels-of-a-string.py
+++ string/reverse-vowels-of-a-string.py
@@ -1,20 +1,22 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        if not s:
-            return s
         l = 0
         r = len(s) - 1
         s = list(s)
+        vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
 
         while l < r:
-            if s[r] in "aeiouAEIOU" and s[l] in "aeiouAEIOU" :
+            if s[r] in vowels and s[l] in vowels :
                 s[r],s[l] = s[l],s[r]
                 r -= 1
                 l += 1
-            elif s[r] not in "aeiouAEIOU":
+
+            elif s[r] not in vowels:
                 r -=1
+
             else:
                 l+=1
+
         return "".join(s)
 
 
