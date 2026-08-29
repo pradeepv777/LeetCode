5ea11ab19e8e17d51078986f04a227ccb3826b43
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-29 12:08:52 +0530

    LC-maximum-number-of-vowels-in-a-substring-of-given-length [Runtime: N/A; Memory: N/A]

--- string/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ string/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,35 +1,27 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
+        if not s:
+            return 0
 
-        curr_str = ""
         vowels = {"a", "e", "i", "o", "u"}
-        counts = 0
+        count = 0
 
+        # Count vowels in the first window size k
         for char in s[:k]:
-            curr_str += char
             if char in vowels:
-                counts += 1
-        max_vowels = counts
+                count += 1
+        max_count = count
 
-        if counts == k:
+        if count == k:
             return k
+        # Slide the window using direct index lookups
+        for r in range(k, len(s)):
+            if s[r] in vowels:
+                count += 1
 
-        for char in s[k:]:
-            outgoing_char = curr_str[0]
-            curr_str = curr_str[1:] + char
+            # Remove chars on the left(at index r - k)
+            if s[r - k] in vowels:
+                count -= 1
+            max_count = max(max_count, count)
 
-            if char in vowels:
-                counts += 1
-
-            if outgoing_char in vowels:
-                counts -= 1
-            max_vowels = max(max_vowels, counts)
-
-        return max_vowels
-
-
-
-
-
-
-        
+        return max_count
