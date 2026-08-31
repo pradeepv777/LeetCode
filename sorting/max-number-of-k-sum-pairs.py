ba95cbc1e316e12e08a0f7d227b8755f16d7390a
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-31 11:18:23 +0530

    LC-max-number-of-k-sum-pairs [Runtime: N/A; Memory: N/A]

--- sorting/max-number-of-k-sum-pairs.py
+++ sorting/max-number-of-k-sum-pairs.py
@@ -1,27 +1,24 @@
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
         nums.sort()
-        if nums[0] > k:
+        if nums and nums[0] > k:
             return 0
 
-        left = 0
-        right = len(nums) - 1
+        left, right = 0, len(nums) - 1
         pairs = 0
 
         while left < right:
             total = nums[left] + nums[right]
+            
             if total == k:
                 pairs += 1
                 left += 1
                 right -= 1
 
             elif total > k:
-                right -=1
+                right -= 1
                 
             else:
-                left +=1
+                left += 1
 
         return pairs
-
-
-        
