a5789ad791e24a66b3af249f05c1d7b31a7eb562
Pradeep Pediredla <114344697+pradeepv777@users.noreply.github.com>
2026-08-31 11:33:10 +0530

    LC-find-the-highest-altitude [Runtime: N/A; Memory: N/A]

--- prefix-sum/find-the-highest-altitude.py
+++ prefix-sum/find-the-highest-altitude.py
@@ -1,11 +1,9 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        res = [0] * len(gain)
-        res[0] = gain[0] # [-5,0,0,0,0]
 
         for i in range(1, len(gain)):
-            res[i] = res[i-1] + gain[i]
+            gain[i] = gain[i-1] + gain[i]
 
-        return max(0,max(res))
+        return max(0,max(gain))
 
         
